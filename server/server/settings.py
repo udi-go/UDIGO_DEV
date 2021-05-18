5c7f6af002e1122ad32373315d4c0b46e5195b31
nameunji <namji117@gmail.com>
2021-05-18 10:39:32 +0900

    nej 20210518 django server setting

--- server/server/settings.py
+++ server/server/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hkb360y#c$a*xrfmp84lg&_=k-_w$zrm)h!1k9kn)5(t+t+hlv'
 SECRET_KEY = server_settings.SECRET['secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
